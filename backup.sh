4b5fab8432bf8c6039a7c47c37c488a07b91652c
jefoh <46411467+jefoh@users.noreply.github.com>
2025-03-12 16:23:36 -0500

    Modified description

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 #
 # Copyright (c) 2025 jefoh
 #
-# This software is licensed under the terms of the MIT license.
+# This software is licensed under the terms of the GNU General Public License v3.0.
 # See the LICENSE file for more details.
 # 
 #
